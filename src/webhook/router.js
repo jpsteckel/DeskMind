a736abc3ae2396199a18b32dd0f9e96716fee01e
jpsteckel <fluidwingz@gmail.com>
2026-05-29 02:32:22 -0400

    removed reference to old function

--- src/webhook/router.js
+++ src/webhook/router.js
@@ -4,7 +4,6 @@ import { handleCallAnswered } from './callAnswered.js';
 import { handleCallEnded } from './callEnded.js';
 import { handleCallRecording } from './callRecording.js';
 import { handleCallAnalyzed } from './callAnalyzed.js';
-import { handleCallTranscript } from './callTranscript.js';
 
 const router = Router();
 
